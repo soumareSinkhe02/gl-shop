89440632fd9b104befcac9416b479c6663ec167e
Sinkhe Soumare <148817678+soumareSinkhe02@users.noreply.github.com>
2026-05-11 01:34:12 +0000

    V3 - LOGIQUE PANIER AJOUT/SUPPRESSION/QUANTITE
    Création tableau panier vide
    ajouterAuPanier() avec find() pour éviter les doublons
    afficherPanier() génère le HTML du panier dynamiquement
    augmenter() et diminuer() modifient la quantité
    filter() supprime l'article si quantité = 0
    reduce() calcule le total automatiquement.

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ const articles = [
     { id: 4, nom: "Clavier", prix: 35000, image: "img/clavier.jpg", description: "Clavier d'ordinateur" },
     { id: 5, nom: "Souris", prix: 14000, image: "img/souris.jpg", description: "Souris Logitech M185 sans fil, caractérisée par sa couleur noire avec des accents rouges et son nano-récepteur USB compact." },
 ];
+// Tableau vide qui contiendra les articles ajoutes au panier
+let panier = [];
 
-function afficherArticles(){
+function afficherArticles() {
     articles.forEach((article) => {
-    document.getElementById("liste-articles").innerHTML += `
+        document.getElementById("liste-articles").innerHTML += `
                         <div class="card mb-3">
                             <div class="card-body">
                                 <div class="row align-items-center">
@@ -28,11 +30,89 @@ function afficherArticles(){
                                     <img src="${article.image}" alt="Laptop" class="img-fluid rounded">
                                 </div>
                                 <div col-12>
-                                    <button class="btn btn-success btn-sm w-100">Ajouter</button>
+                                    <button class="btn btn-success btn-sm w-100" onclick="ajouterAuPanier(${article.id})">Ajouter</button>
                                 </div>
                             </div>
                         </div>
     `;
-});
+    });
 }
 afficherArticles();
+
+function ajouterAuPanier(id) {
+    // On cherche si l'article existe deja dans le panier
+    // find() cherche dans le tableau panier si un article avec cet id existe deja
+    // Si trouve -> articleExistant contient l'objet, sinon -> undefined
+    let articleExitant = panier.find(p => p.id === id);
+    if (articleExitant) {
+        // Si oui -> L'article est deja dans le panier, on augmente judte sa quantite
+        articleExitant.quantite++;
+    } else {
+        // L'article n'est pas encore dans le panier
+        // On le cherche d'abord dans le tableau articles avec find()
+        let article = articles.find(a => a.id === id);
+        // ...article copie toutes les proprietes (nom, prix, image...)
+        // et on ajoute quantite:1 car c'est le premier ajout
+        panier.push({ ...article, quantite: 1 });
+    }
+    //On reaffiche le panier apres chaque modification
+    afficherPanier();
+}
+function afficherPanier() {
+    let panierId = document.getElementById("panier-items");
+    // On vide d'abord le panier affiche pour eviter les doublons
+    panierId.innerHTML = "";
+
+    // On parcourt chaque article du panier
+    panier.forEach((item) => {
+        panierId.innerHTML += `
+            <div class="row align-items-center mb-2">
+                <div class="col-6">
+                    <p>${item.nom}</p>
+                </div>
+                <div class="col-6 d-flex align-items-center justify-content-end gap-2">
+                    <!-- On passe l'id au bouton pour savoir quel article modifier -->
+                    <button class="btn btn-sm btn-outline-secondary" onclick="diminuer(${item.id})">-</button>
+                    <span>${item.quantite}</span>
+                    <button class="btn btn-sm btn-outline-secondary" onclick="augmenter(${item.id})">+</button>
+                </div>
+            </div>        
+        `;
+
+        // reduce() parcourt le panier et additionne prix * quantite de chaque article
+        // acc = accumulateur (le total en cours), commence a 0
+        // Tour 1 -> acc=0 + (125000*1) = 125000
+        // Tour 2 -> acc=125000 + (14000*2) = 153000
+        let total = panier.reduce((acc, item) => acc + (item.prix * item.quantite), 0);
+
+        // On affiche le total calcule
+        document.getElementById("total-prix").innerHTML = total + " FCFA"
+    });
+}
+function augmenter(id) {
+    // On trouve l'article dans le panier par son id
+    let item = panier.find(p => p.id === id);
+    // On augmente sa quantite
+    item.quantite++;
+    // On reaffiche le panier mis a jour
+    afficherPanier();
+}
+
+function diminuer(id) {
+    let item = panier.find(p => p.id === id);
+    if (item.quantite > 1) {
+        // Si quantite > 1 on diminue juste
+        item.quantite--;
+    } else {
+        // Si quantite = 1 et on diminue -> on supprime l'article du panier
+        // filter() garde seulement les articles dont l'id est DIFFERENT
+        panier = panier.filter(p => p.id !== id);
+    }
+    // On reaffiche le panier mis a jour
+    afficherPanier();
+}
+
+// find()   → cherche UN élément selon une condition
+// push()   → ajoute un élément à la fin du tableau
+// filter() → garde les éléments qui respectent la condition
+// reduce() → réduit un tableau à une seule valeur (ex: total)
